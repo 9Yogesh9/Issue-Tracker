86c3bdea4185fbd3900a086ff171c38d8a5de86d
Yogesh Bhat <yogesh99bhat@gmail.com>
2023-01-19 01:09:01 +0530

    Shifted database to atlas cloud mongodb

--- config/mongoose.js
+++ config/mongoose.js
@@ -4,7 +4,8 @@ mongoose.set('strictQuery', false);
 launch().catch(err => console.log(`Error talking to MongoDB : ${err}`));
 
 async function launch(){
-    await mongoose.connect("mongodb://127.0.0.1:27017/issue_tracker");
+    // await mongoose.connect("mongodb://127.0.0.1:27017/issue_tracker");
+    await mongoose.connect("mongodb+srv://Test_User:Test1234@clusterissuetracker.abpbpqo.mongodb.net/issue_tracker");
 }
 
 const db = mongoose.connection.once('open', ()=>{
