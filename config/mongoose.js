74e6e3fded4bba30b4aa876b70b9696b508608bf
Yogesh Bhat <yogesh99bhat@gmail.com>
2023-01-19 01:24:36 +0530

    Environment variables set

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 mongoose.set('strictQuery', false);
+const URI = process.env.DB_URI;
 
 launch().catch(err => console.log(`Error talking to MongoDB : ${err}`));
 
 async function launch(){
     // await mongoose.connect("mongodb://127.0.0.1:27017/issue_tracker");
-    await mongoose.connect("mongodb+srv://Test_User:Test1234@clusterissuetracker.abpbpqo.mongodb.net/issue_tracker");
+    await mongoose.connect(URI);
 }
 
 const db = mongoose.connection.once('open', ()=>{
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const express = require("express");
 const port = 8000;
 const app = express();
